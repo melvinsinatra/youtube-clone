8ebc4b9bb83f6c246708f15c765c70dde6261632
Melvin Sinatra <melvin.sinatra2002@gmail.com>
2022-10-11 00:30:08 +0700

    adjust spacing

--- src/pages/search-page.jsx
+++ src/pages/search-page.jsx
@@ -6,23 +6,23 @@ import { useTheme } from '../contexts/ThemeContext';
 import MobileSidebar from '../components/MobileSidebar';
 import SearchVideoResults from '../layouts/SearchVideoResults';
 
-const SearchContainer = styled(Container)(({theme}) => ({
+const SearchContainer = styled(Container)(({ theme }) => ({
 	[theme.breakpoints.down('md')]: {
 		padding: '1em'
 	}
-}))
+}));
 
 const SearchPage = () => {
 	const { query } = useParams();
-  const theme = useTheme();
+	const theme = useTheme();
 
 	return (
 		<Box>
 			<Stack direction="row" marginTop={{ xs: 7, md: 12 }}>
 				{/* If window size is medium or larger, use <Sidebar/> Else use <MobileSidebar/> */}
 				{window.innerWidth >= theme.breakpoints.values.md ? <Sidebar /> : <MobileSidebar />}
-				<SearchContainer disableGutters={{xs: 'true', md: 'false'}}>
-					<SearchVideoResults searchQuery={query}/>
+				<SearchContainer disableGutters={{ xs: 'true', md: 'false' }}>
+					<SearchVideoResults searchQuery={query} />
 				</SearchContainer>
 			</Stack>
 		</Box>
