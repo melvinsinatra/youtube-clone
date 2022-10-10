c4a8e565167f7df18250d5301eef6afdbb1b9447
Melvin Sinatra <melvin.sinatra2002@gmail.com>
2022-10-10 22:59:16 +0700

    remove console log

--- src/layouts/FeedVideoResults.jsx
+++ src/layouts/FeedVideoResults.jsx
@@ -33,8 +33,6 @@ const FeedVideoResults = () => {
 		};
 	}, [category]);
 
-	console.log(results)
-
 	const cardEls = results?.map((data) => {
 		return (
 			<Grid item xs={12} md={6} lg={4} xl={2.4} marginBottom={{ xs: 1 }}>
